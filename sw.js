e5e23ec5ffbe4aaa9a5616b7865fe6a89c09eec3
maximilianbauer43-max <maximilianbauer43@gmail.com>
2025-11-18 22:43:34 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open("notam-cache").then(cache => {
+    return cache.addAll(["index.html", "manifest.json"]);
+  }));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
